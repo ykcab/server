e47268af337831269fb2492d26849737315d0579
Roeland Jago Douma <roeland@famdouma.nl>
2019-10-28 19:22:05 +0100

    Allow internal shares to have a default expiration date
    * Needs admin UI
    * Needs more checks
    * Basics seem to work
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/Share20/DefaultShareProvider.php
+++ lib/private/Share20/DefaultShareProvider.php
@@ -142,9 +142,19 @@ class DefaultShareProvider implements IShareProvider {
 		if ($share->getShareType() === \OCP\Share::SHARE_TYPE_USER) {
 			//Set the UID of the user we share with
 			$qb->setValue('share_with', $qb->createNamedParameter($share->getSharedWith()));
+
+			//If an expiration date is set store it
+			if ($share->getExpirationDate() !== null) {
+				$qb->setValue('expiration', $qb->createNamedParameter($share->getExpirationDate(), 'datetime'));
+			}
 		} else if ($share->getShareType() === \OCP\Share::SHARE_TYPE_GROUP) {
 			//Set the GID of the group we share with
 			$qb->setValue('share_with', $qb->createNamedParameter($share->getSharedWith()));
+
+			//If an expiration date is set store it
+			if ($share->getExpirationDate() !== null) {
+				$qb->setValue('expiration', $qb->createNamedParameter($share->getExpirationDate(), 'datetime'));
+			}
 		} else if ($share->getShareType() === \OCP\Share::SHARE_TYPE_LINK) {
 			//set label for public link
 			$qb->setValue('label', $qb->createNamedParameter($share->getLabel()));
